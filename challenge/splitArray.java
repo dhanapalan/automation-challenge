221435d2d47239ff0a9a8ff914e587a042f69ccc
dhanapalan <c.dhanapalan@gmail.com>
2022-11-16 16:37:25 +0530

    code updated

--- challenge/splitArray.java
+++ challenge/splitArray.java
@@ -1,39 +1,69 @@
-import java.util.*;
- 
+
 class splitArray {
- 
 
-    public static int findMinRec(int arr[], int i, int sumCalculated, int sumTotal)
-    {
-   
-        if (i == 0)  return Math.abs((sumTotal - sumCalculated) - sumCalculated);
- 
-  
-        return Math.min(
-            findMinRec(arr, i - 1,
-                       sumCalculated + arr[i - 1],
-                       sumTotal),
-            findMinRec(arr, i - 1, sumCalculated,
-                       sumTotal));
-    }
- 
-    public static int findMin(int arr[], int n)
-    {
+	// Returns split point. If not possible, then
+	// return -1.
+	static int findSplitPoint(int arr[], int n)
+	{
+	
+	// traverse array element and compute sum
+	// of whole array
+	int leftSum = 0;
+	
+	for (int i = 0 ; i < n ; i++)
+		leftSum += arr[i];
 
-        int sumTotal = 0;
-        for (int i = 0; i < n; i++)
-            sumTotal += arr[i];
- 
-        return findMinRec(arr, n, 0, sumTotal);
-    }
- 
-    public static void main(String[] args)
-    {
-        int arr[] = { 3, 1, 4, 2, 2, 1 };
-        int n = arr.length;
-        System.out.print("The minimum difference"
-                         + " between two sets is "
-                         + findMin(arr, n));
-    }
+	// again traverse array and compute right
+	// sum and also check left_sum equal to
+	// right sum or not
+	int rightSum = 0;
+	
+	for (int i = n-1; i >= 0; i--)
+	{
+		// add current element to right_sum
+		rightSum += arr[i];
+
+		// exclude current element to the left_sum
+		leftSum -= arr[i] ;
+
+		if (rightSum == leftSum)
+			return i ;
+	}
+
+	// if it is not possible to split array
+	// into two parts.
+	return -1;
+	}
+
+	// Prints two parts after finding split
+	// point using findSplitPoint()
+	static void printTwoParts(int arr[], int n)
+	{
+		int splitPoint = findSplitPoint(arr, n);
+	
+		if (splitPoint == -1 || splitPoint == n )
+		{
+			System.out.println("Not Possible" );
+			return;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if(splitPoint == i)
+				System.out.println();
+				
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	// Driver program
+	public static void main (String[] args) {
+	
+	int arr[] = {1 , 2 , 3 ,8, 4 , 5 , 5,7 };
+	int n = arr.length;
+	
+	printTwoParts(arr, n);
+		
+	}
 }
- 
+
+
